cb892863564764bc84e01d10064a4df89701d0fa
SyedMahamudulHasan <syedhasan.cse@gmail.com>
2022-11-08 18:11:14 +0600

    added trip loaded state

--- lib/blocs/app_bloc.dart
+++ lib/blocs/app_bloc.dart
@@ -4,8 +4,7 @@ import 'package:todo/blocs/app_state.dart';
 import 'package:todo/data/repogitori/repository.dart';
 
 class TripBloc extends Bloc<TripEvent, TripState> {
-  final TripRepository _tripRepository;
-  TripBloc(this._tripRepository) : super(TripLoadingState()) {
+  TripBloc() : super(TripLoadingState()) {
     on<LoadTripEvent>((event, emit) async {
       emit(TripLoadingState());
     });
